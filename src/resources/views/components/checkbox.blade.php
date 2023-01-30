ebb3e0416ba661f516a1068a65f2b5934aa65e1e
Galtsev Timofey <galtsev.t@yandex.ru>
2023-01-30 21:40:54 +0700

    fix

--- src/resources/views/components/checkbox.blade.php
+++ src/resources/views/components/checkbox.blade.php
@@ -1,5 +1,5 @@
 <div class="form-check">
-    <input name="{{ $name ?? '' }}" class="form-check-input" type="checkbox" value="{{ $value ?? '' }}" id="check_{{ $name ?? '' }}" {{ $checked }}>
+    <input name="{{ $name ?? '' }}" class="form-check-input" type="checkbox" value="{{ $value ?? '' }}" id="check_{{ $name ?? '' }}" {{ $checked ?? null }}>
     <label class="form-check-label" for="check_{{ $name ?? '' }}">
         {{ $labelName ?? 'checkbox' }}
     </label>
